3a4cab99e0f99ef41ec009660f98a42da5951b86
Maas Lalani <maas@live.ca>
2016-10-10 14:28:13 -0400

    Minor formatting changes

--- js/script.js
+++ js/script.js
@@ -2,25 +2,24 @@ window.onload = function() {
 
     document.getElementById("load-articles").addEventListener('click', function ()
     {
+        var hidden_articles = document.getElementsByClassName('hidden');
 
-    var hidden_articles = document.getElementsByClassName('hidden');
+        // Since the array shrinks over time we need the original length.
+        var hidden_articles_length = hidden_articles.length;
 
-    // Since the array shrinks over time we need the original length.
-    var hidden_articles_length = hidden_articles.length;
+        for (var i = 0; i < hidden_articles_length; i++)
+        {
+            /* 
+            Each iteration we must show the first element in the array because each iteration
+            results in the array becoming smaller by removing the first element.
+            That is why we use [0] rather than [i]
+            */
 
-    for (var i = 0; i < hidden_articles_length; i++)
-    {
-        /* 
-        Each iteration we must show the first element in the array because each iteration
-        results in the array becoming smaller by removing the first element.
-        That is why we use [0] rather than [i]
-        */
+            // Removes the all classes except for 'question'
+            hidden_articles[0].className = "question";
+        }
 
-        // Removes the all classes except for 'question'
-        hidden_articles[0].className = "question";
-    }
-
-    // Hide the load-articles button
-    this.className = "hidden";
- }); 
+        // Hide the load-articles button
+        this.className = "hidden";
+    }); 
 }
